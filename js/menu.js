32bedb658b4d0b1671908c9f4fc8e8e12ceeb120
Sebastian Mere <sebastian.mere@gmail.com>
2023-07-09 20:38:39 -0300

    Mediaqueries en seccion nosotros

--- js/menu.js
+++ js/menu.js
@@ -11,10 +11,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const nav = document.querySelector("nav");
-
-hamburgerMenu.addEventListener("click", function () {
-    nav.classList.toggle("open");
-});
