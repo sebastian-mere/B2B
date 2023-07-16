85f7a18f7227d15e324fa2c214d0c11e7c8e43f2
Sebastian Mere <sebastian.mere@gmail.com>
2023-07-16 12:14:09 -0300

    planes desplegables

---
+++ js/planes.js
@@ -0,0 +1,11 @@
+var planContainers = document.querySelectorAll('.plan')
+
+planContainers.forEach(function(container) {
+    var titulo = container.querySelector('.titular');
+
+  titulo.addEventListener('click', function() {
+
+    container.classList.toggle('activo');
+  });
+
+});
